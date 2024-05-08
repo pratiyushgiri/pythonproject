15469a5af143c911dde3937d3c39b2393c2c595b
unknown <gauravgiri078@gmail.com>
2024-05-08 08:27:35 +0545

    new functionality added

--- backend.py
+++ backend.py
@@ -1,2 +1,6 @@
 def hello_world(name):
     return "Hello" + name
+
+def handle_request(request):
+    response = "This is out nice response"
+    return response
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 ##Python Project 2
 
 This is a simple project to learn git.
-
-
+Add new line in ReadMe
 Author: User
